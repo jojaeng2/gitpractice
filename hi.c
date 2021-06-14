ab9e38b21d7b588f34d9cdb83016d942eb8a5a5f
Henu <hyun0404woo@gmail.com>
2021-06-14 16:16:14 +0900

    return hello1

--- hi.c
+++ hi.c
@@ -1,16 +1,7 @@
 #include <stdio.h>
 
-
-
 int main(){
-    int a = 10;
-    int b = 5;
-    
-    printf("hello1\n");
-    printf("hello2\n");
-    printf("hello3\n");
-    printf("hello4\n");
-    printf("hello5\n");
-    
+    printf("hi\n");
+
     return 0;
 }

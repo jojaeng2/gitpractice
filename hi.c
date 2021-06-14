e57625234df3a9d7a7cd6e098aff6f8e1e897002
Henu <hyun0404woo@gmail.com>
2021-06-14 16:33:28 +0900

    Revert "return ab"
    This reverts commit cafd4204069a2591bc4eaf8649575fa7485f715a.

--- hi.c
+++ hi.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 
-
+typedef struct node{
+    int value;
+    struct node* next;
+}Node;
 
 int main(){
-    int a = 10;
-    int b = 5;
+    Node n1;
+    n1.value = 1;
+    n1.next = NULL;
     
-    printf("hello1\n");
-    printf("hello2\n");
-    printf("hello3\n");
-    printf("hello4\n");
-    printf("hello5\n");
-    
+
     return 0;
 }

d6aa41e07563f90324e2f1e49602efa563693505
Henu <hyun0404woo@gmail.com>
2021-06-14 16:48:08 +0900

    reset

--- hi.c
+++ hi.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+typedef struct node{
+    int value;
+    int x, y;
+    struct node* next;
+}Node;
+
+int main(){
+    Node* nodes;
+    
+    nodes = (Node*) malloc(sizeof(nodes) * 10);
+    
+    
+    
+    free(nodes);
+
+    return 0;
+}
